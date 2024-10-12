6e7ab0bac9ca39ab194d4cc3cbf3645450715814
Théo Vitor Vanni Olivares Tavora <theovannitavora@gmail.com>
2024-10-12 21:45:32 +0000

    ADDED: bootstrap on Criofitness.html

--- script.js
+++ script.js
@@ -3,13 +3,13 @@
 document.getElementById('menu-icon').addEventListener('click', function () {
   const menu = document.querySelector('.menu');
   const boxicon = document.getElementById('menu-icon');
-  
+
   // Alterna a classe 'open' no menu
   menu.classList.toggle('open');
-  
+
   // Verifica se o menu está aberto
   const isOpen = menu.classList.contains('open');
-  
+
   // Ajusta a margem inferior do ícone com base no estado do menu
   boxicon.style.marginBottom = isOpen ? '50%' : '0'; // Use '50px' ou ajuste conforme necessário
 });
@@ -17,7 +17,7 @@ document.getElementById('menu-icon').addEventListener('click', function () {
 
 // Menu para clicável (Criofitness)
 let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let navbar = document.querySelector('.navigation-bar');
 
 menu.onclick = () => {
   menu.classList.toggle('bx-x');
