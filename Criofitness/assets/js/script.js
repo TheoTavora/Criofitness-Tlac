867bfaacf86027cc796dd5470ba76023e4e3dafe
Théo Vitor Vanni Olivares Tavora <theovannitavora@gmail.com>
2024-11-04 22:11:27 +0000

    ADD: Function to fetch HTML

--- Criofitness/assets/js/script.js
+++ Criofitness/assets/js/script.js
@@ -13,3 +13,17 @@ window.addEventListener('scroll', function () {
     var header = document.querySelector('header');
     header.classList.toggle('scrolled', window.scrollY > 30);
 });
+
+fetch('planobasico.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('plano-container-basico').innerHTML = data;
+    })
+    .catch(error => console.error('Erro ao carregar o arquivo:', error));
+
+fetch('planoclube.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('plano-container-clube').innerHTML = data;
+    })
+    .catch(error => console.error('Erro ao carregar o arquivo:', error));
